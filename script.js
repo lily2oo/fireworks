df0d245aa3fa3081fcfaa7d1219d58f2ce4e4a5d
須古純平 <Lily2oo@sukojunpeinoMacBook-Pro.local>
2022-06-24 20:50:17 +0900

    a

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function init() {
     // safari用。DeviceOrientation APIの使用をユーザに許可して貰う
     // permitDeviceOrientationForSafari();
     document.getElementById("test").innerHTML = "SP";
-    window.addEventListener("load", permitDeviceOrientationForSafari);
+    permitDeviceOrientationForSafari();
 
     window.addEventListener(
       "deviceorientation",
